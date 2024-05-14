089324f40f99d10acd3f15b31c0c72863a1885af
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-14 19:42:44 +0000

    alpabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The excecution begines here
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
