ac2bae80b8c024f938ecfa4759a0c753a7912688
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-14 19:48:51 +0000

    adding caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The excecution begines here
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
