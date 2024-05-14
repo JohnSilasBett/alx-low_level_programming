b59228d2e6b3774b87b658efb61b236da3cf25d5
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-14 15:32:21 +0000

    Must and must do it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
-	for (i=0; str[i]!='\0'; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
 		putchar(str[i]);
+	putchar('\n');
 	return (1);
 }
