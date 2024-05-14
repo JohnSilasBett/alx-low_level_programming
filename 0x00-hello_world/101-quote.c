9797873535e4a127f981b071e83a219c750c90e6
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-14 03:39:41 +0000

    using putchach

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints "That piece of art is useful"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
