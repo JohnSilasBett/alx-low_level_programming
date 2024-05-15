bc011945e465a3fa923107c32e555f98089ad177
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-15 08:41:30 +0000

    1-10 separated by , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - where code excecution begin
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar (32);
+	}
+	putchar(10);
+	return (0);
+}
