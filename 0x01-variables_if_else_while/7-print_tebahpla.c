83bf4e49cf6532abb384d836ce4d666982055fc5
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-15 08:06:19 +0000

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - where code excecution begin
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
