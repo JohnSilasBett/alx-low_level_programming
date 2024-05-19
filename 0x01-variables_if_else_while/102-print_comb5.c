3272f72881c070671dd9447dcf7545a34be4f48d
JohnSilasBett <johnsailasbett@outlook.com>
2024-05-19 12:54:32 +0000

    the final

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - where code excecution begin
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar(' ');
+			if (i != 98 || j != 99)
+			{
+				putchar(44);
+				putchar (32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
